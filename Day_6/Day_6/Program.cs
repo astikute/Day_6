842ac6fd071677bf477e732930a06bec85af4e09
astikute <agnese.stikute@gmail.com>
2018-05-11 21:18:20 +0300

    6.diena
    Cikli (for, while, do-while)

--- Day_6/Day_6/Program.cs
+++ Day_6/Day_6/Program.cs
@@ -10,6 +10,48 @@ namespace Day_6
     {
         static void Main(string[] args)
         {
+            // i++  i=i+1
+            int vertiba = 4;
+            for (int i = 0; i < vertiba; i++) //nosacijums
+            {
+                Console.WriteLine(i);
+            }
+
+            int skaititajs = 0;
+            while (skaititajs!=5)
+            {
+                Console.WriteLine(skaititajs);
+                skaititajs++;
+            }
+
+            string izvele = "";
+            double rezultats;
+            while  (izvele != "iziet") // nosacijumu parbauda sakuma!
+            {
+                Console.WriteLine("Saskaitit vai iziet?");
+                izvele = Console.ReadLine();
+
+                if (izvele == "saskaitit")
+                {
+                    Console.WriteLine("Ievadiet: ");
+                    double sk1 = Convert.ToDouble(Console.ReadLine());
+                    double sk2 = Convert.ToDouble(Console.ReadLine());
+                    rezultats = Calculator(sk1, sk2);
+                    Console.WriteLine("Rezultats: " + rezultats);
+                }
+            }
+
+            do // nosacijumu parbauda beigas! Vismaz vienu reizi ciklu izpildis!
+            {
+
+            }
+            while (izvele != "iziet");
+
+            Console.ReadLine();
+        }
+        static double Calculator (double a, double b)
+        {
+            return a + b;
         }
     }
 }
